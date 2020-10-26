57f3929643170e5391cc48efcf3df11cd9b5d07e
Jeff Brewer <jeffreybrewer2019@u.northwestern.edu>
2020-10-26 16:09:35 -0600

    add audio

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ var canvas = document.getElementById('c');
 canvas.width = window.innerWidth*window.devicePixelRatio;
 canvas.height = window.innerHeight*window.devicePixelRatio;
 
+//initialize audio analyser variable
+var fft = null;
+
 function main(){
 	//setup gl and uniforms
 	setup_gl();
@@ -17,7 +20,7 @@ function main(){
 	u_ProjMatrix = gl.getUniformLocation(gl.program, 'u_ProjMatrix');
 
 	fovy = 50;
-	view_matrix.set_camera([0, 0, 6], [0, 0, 0], [0, 1, 0]);
+	view_matrix.set_camera([0, 0, 7], [0, 0, 0], [0, 1, 0]);
 	proj_matrix.set_perspective(fovy, canvas.width/canvas.height, .01, 500);
 	gl.uniformMatrix4fv(u_ModelMatrix, false, model_matrix.e);
 	gl.uniformMatrix4fv(u_ViewMatrix, false, view_matrix.e);
@@ -35,14 +38,23 @@ function main(){
 		last_t = this_t;
 
 		//update and draw visualizations
-		if(elapsed < 500){
-			iso.update(elapsed);
+		if(elapsed < 500 && fft != null){
+			fft.get_data();
+			iso.update(elapsed, fft);
 			iso.draw();
 		}
 
 		requestAnimationFrame(tick, canvas);
 	}
 	tick();
+}
+
+//initialize audio analyser
+document.body.onmousedown = function(){
+	if(fft == null){
+		fft = new FFT(4096, .5, 'Underground - NITRXMANE.mp3');
+		fft.play_audio();
+	}
 }
 
 //handle window resizing
