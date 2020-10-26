003cabb4b6f673ee23688714fba513f972fcc46e
Jeff Brewer <jeffreybrewer2019@u.northwestern.edu>
2020-10-25 19:00:23 -0600

    added program switching

--- gl.js
+++ gl.js
@@ -1,3 +1,6 @@
+var programs = [];
+var p_fpv = 3;
+
 //load a shader from string
 function load_shader(type, source){
 	let s = gl.createShader(type);
@@ -15,4 +18,20 @@ function create_program(vs, fs){
 	gl.attachShader(p, f);
 	gl.linkProgram(p);
 	return p;
+}
+
+//switch to another shader program
+function switch_shader(i){
+	gl.useProgram(programs[i]);
+	gl.program = programs[i];
+}
+
+//initialize gl context
+function setup_gl(){
+	gl = document.getElementById('c').getContext('webgl');
+	gl.enable(gl.BLEND);
+	gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+	gl.clearColor(0, 0, 0, 0);
+
+	programs.push(create_program(document.getElementById('v_iso').text, document.getElementById('f_iso').text));
 }
