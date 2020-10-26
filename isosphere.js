21dd8d690b769ec2bd1ee38c87a985f585542265
Jeff Brewer <jeffreybrewer2019@u.northwestern.edu>
2020-10-25 19:07:54 -0600

    made isosphere radius 1.0

--- isosphere.js
+++ isosphere.js
@@ -10,16 +10,17 @@ function gen_iso(iter){
 			 [1,5,9],[5,11,4],[11,10,2],[10,7,6],[7,1,8],
 			 [3,9,4],[3,4,2],[3,2,6],[3,6,8],[3,8,9],
 			 [4,9,5],[2,4,11],[6,2,10],[8,6,7],[9,8,1]];
-	let r = 1.9;
+	for(let i = 0; i < v.length; i++)
+		v[i] = norm(v[i]);
 
 	//iteratively convert every one triangle into 4 and normalize vertices
 	for(let i = 0; i < iter; i++){
 		let n_v = [];
 		let n_t = [];
 		for(let j = 0; j < t.length; j++){
-			let a = resize(midpoint(v[t[j][0]], v[t[j][1]]), r);
-			let b = resize(midpoint(v[t[j][1]], v[t[j][2]]), r);
-			let c = resize(midpoint(v[t[j][0]], v[t[j][2]]), r);
+			let a = norm(midpoint(v[t[j][0]], v[t[j][1]]));
+			let b = norm(midpoint(v[t[j][1]], v[t[j][2]]));
+			let c = norm(midpoint(v[t[j][0]], v[t[j][2]]));
 			let l = n_v.length;
 
 			for(let k = 0; k < 3; k++)
