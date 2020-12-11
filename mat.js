d6c7ffaab1312802068553036054e96a78e379b4
Jeff <jeff@Jeffs-MacBook-Pro.local>
2020-12-11 15:15:34 -0700

    init git

---
+++ mat.js
@@ -0,0 +1,115 @@
+//4x4 matrix object constructor
+var Mat4 = function(){
+	this.e = new Float32Array([1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1]);
+}
+
+//multiply by other matrix from the right
+Mat4.prototype.mult = function(o){
+	for(let i = 0; i < 4; i++){
+		let av = [this.e[i+0],this.e[i+4],this.e[i+8],this.e[i+12]];
+		this.e[i+0] = dot(av, o.e.slice(0, 4));
+		this.e[i+4] = dot(av, o.e.slice(4, 8));
+		this.e[i+8] = dot(av, o.e.slice(8, 12));
+		this.e[i+12]= dot(av, o.e.slice(12));
+	}
+}
+
+//reset matrix to identity matrix
+Mat4.prototype.set_identity = function(){
+	this.e = new Float32Array([1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1]);
+}
+
+//create persepctive matrix
+Mat4.prototype.set_perspective = function(fovy, aspect, near, far){
+	fovy = Math.PI*fovy/180/2;
+	rd = 1/(far-near);
+	ct = Math.cos(fovy)/Math.sin(fovy);
+
+	this.e[0] = ct/aspect;
+	this.e[1] = 0;
+	this.e[2] = 0;
+	this.e[3] = 0;
+	this.e[4] = 0;
+	this.e[5] = ct;
+	this.e[6] = 0;
+	this.e[7] = 0;
+	this.e[8] = 0;
+	this.e[9] = 0;
+	this.e[10]= -(far+near)*rd;
+	this.e[11]= -1;
+	this.e[12]= 0;
+	this.e[13]= 0;
+	this.e[14]= -2*near*far*rd;
+	this.e[15]= 0;
+}
+
+//create view matrix
+Mat4.prototype.set_camera = function(camera, focus, up){
+	let f = norm(sub(focus, camera));
+	let s = norm(cross3(f, up));
+	let u = cross3(s, f);
+
+	this.e[0] = s[0];
+	this.e[1] = u[0];
+	this.e[2] = -f[0];
+	this.e[3] = 0;
+	this.e[4] = s[1];
+	this.e[5] = u[1];
+	this.e[6] = -f[1];
+	this.e[7] = 0;
+	this.e[8] = s[2];
+	this.e[9] = u[2];
+	this.e[10]= -f[2];
+	this.e[11]= 0;
+	this.e[12]= 0;
+	this.e[13]= 0;
+	this.e[14]= 0;
+	this.e[15]= 1;
+
+	this.translate(-camera[0], -camera[1], -camera[2]);
+}
+
+//scale transformation matrix in x, y, z
+Mat4.prototype.scale = function(x, y, z){
+	this.e[0] *= x;
+	this.e[1] *= x;
+	this.e[2] *= x;
+	this.e[3] *= x;
+	this.e[4] *= y;
+	this.e[5] *= y;
+	this.e[6] *= y;
+	this.e[7] *= y;
+	this.e[8] *= z;
+	this.e[9] *= z;
+	this.e[10] *= z;
+	this.e[11] *= z;
+}
+
+//translate transformation matrix by x, y, z
+Mat4.prototype.translate = function(x, y, z){
+	this.e[12] += this.e[0]*x + this.e[4]*y + this.e[8]*z;
+	this.e[13] += this.e[1]*x + this.e[5]*y + this.e[9]*z;
+	this.e[14] += this.e[2]*x + this.e[6]*y + this.e[10]*z;
+	this.e[15] += this.e[3]*x + this.e[7]*y + this.e[11]*z;
+}
+
+//rotate transformation matrix by angle in degrees on axis [x, y, z]
+Mat4.prototype.rotate = function(angle, axis){
+	angle = Math.PI/180*angle;
+	axis = norm(axis);
+
+	let x = axis[0];
+	let y = axis[1];
+	let z = axis[2];
+	let c = Math.cos(angle);
+	let s = Math.sin(angle);
+	let nc = 1-c;
+	let r = {
+		e: new Float32Array([x*x*nc+c, x*y*nc+z*s, z*x*nc-y*s, 0,
+							 x*y*nc-z*s, y*y*nc+c, y*z*nc+x*s, 0,
+							 z*x*nc+y*s, y*z*nc-x*s, z*z*nc+c, 0,
+							 0, 0, 0, 1])
+	};
+
+	this.mult(r);
+}
---
+++ rain_vis.js
@@ -0,0 +1,165 @@
+//matrix rain visualization
+class Matrix_Rain{
+	constructor(p_fpv, num, len){
+		this.p_fpv = p_fpv;
+		this.num = num + (num % 2);
+		this.len = len;
+		this.warp = 0;
+		this.rotation = 0;
+		this.rot_time = 0;
+		this.switch_time = 0;
+
+		let s = .03;
+		this.symbols = [[-.5*s,s,0, .5*s,s,0,
+						 -.5*s,s,0, 0,-s,0,
+						 .5*s,s,0, 0,-s,0],
+						[-.5*s,s,0, .5*s,s,0,
+						 -.5*s,-s,0, .5*s,-s,0,
+						 0,s,0, 0,-s,0],
+						 [-.5*s,-s,0, .5*s,-s,0,
+						 -.5*s,-s,0, 0,s,0,
+						 .5*s,-s,0, 0,s,0]];
+
+		this.sym_len = this.symbols[0].length;
+		this.offsets = [];
+		for(let i = 0; i < len; i++){
+			this.offsets.push(exp_map(i, [0, len], [.1, .005], .25));
+		}
+
+		let vertex_length = num*len*this.sym_len;
+		this.pos_buffer = new Float32Array(vertex_length*this.p_fpv);
+		this.off_buffer = new Float32Array(vertex_length);
+		this.opc_buffer = new Float32Array(vertex_length);
+		this.ang_buffer = new Float32Array(vertex_length);
+		this.fsize = this.pos_buffer.BYTES_PER_ELEMENT;
+
+		this.strands = [];
+		this.curr_sym = [];
+		this.st_b = [40, 120];
+
+		let r_b = [1.75, 7]; 
+		let a_b = [Math.PI*.6, Math.PI*2.4];
+		let h_b = [7, -7];
+		let pos_ind = 0;
+		let buf_ind = 0;
+		for(let n = 0; n < num; n++){
+			this.strands.push([Math.floor(Math.random()*2*len), map(Math.random(), [0, 1], this.st_b), Math.random()*this.st_b[0]]);
+			let noise_pos = n > num/2 ? n % (num/2) : -n;
+			let r = map(noise.simplex2(noise_pos*.1, 0), [-1, 1], r_b);
+			let a = map(noise.simplex2(noise_pos*.4, 0), [-1, 1], a_b);
+			let pos = [Math.cos(a)*r, 0, Math.sin(a)*r];
+			for(let l = 0; l < len; l++){
+				let center_pos = add(pos, [0, map(l, [0, len], h_b), 0]);
+				let angle = map(l, [0, len], [0, Math.PI*10]);
+				for(let p = 0; p < 2; p++){
+					let symbol = 0;
+					this.curr_sym.push(symbol);
+					for(let i = 0; i < this.symbols[symbol].length; i++, pos_ind += 3, buf_ind++){
+						let vertex_pos = add(this.symbols[symbol].slice(i*3, i*3+3), center_pos);
+						this.pos_buffer[pos_ind + 0] = vertex_pos[0];
+						this.pos_buffer[pos_ind + 1] = vertex_pos[1];
+						this.pos_buffer[pos_ind + 2] = vertex_pos[2];
+
+						this.off_buffer[buf_ind] = p == 0 ? -.03 : .03;
+						this.opc_buffer[buf_ind] = 0;
+						this.ang_buffer[buf_ind] = angle;
+					}
+				}
+			}
+		}
+
+		this.gl_pos_buf = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.gl_pos_buf);
+		gl.bufferData(gl.ARRAY_BUFFER, this.pos_buffer, gl.STATIC_DRAW);
+		this.a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+		gl.vertexAttribPointer(this.a_Position, this.p_fpv, gl.FLOAT, false, this.fsize*this.p_fpv, 0);
+		gl.enableVertexAttribArray(this.a_Position);
+
+		this.gl_off_buf = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.gl_off_buf);
+		gl.bufferData(gl.ARRAY_BUFFER, this.off_buffer, gl.DYNAMIC_DRAW);
+		this.a_Offset = gl.getAttribLocation(gl.program, 'a_Offset');
+		gl.vertexAttribPointer(this.a_Offset, 1, gl.FLOAT, false, this.fsize, 0);
+		gl.enableVertexAttribArray(this.a_Offset);
+
+		this.gl_opc_buf = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.gl_opc_buf);
+		gl.bufferData(gl.ARRAY_BUFFER, this.opc_buffer, gl.DYNAMIC_DRAW);
+		this.a_Opacity = gl.getAttribLocation(gl.program, 'a_Opacity');
+		gl.vertexAttribPointer(this.a_Opacity, 1, gl.FLOAT, false, this.fsize, 0);
+		gl.enableVertexAttribArray(this.a_Opacity);
+
+		this.gl_ang_buf = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.gl_ang_buf);
+		gl.bufferData(gl.ARRAY_BUFFER, this.ang_buffer, gl.STATIC_DRAW);
+		this.a_Angle = gl.getAttribLocation(gl.program, 'a_Angle');
+		gl.vertexAttribPointer(this.a_Angle, 1, gl.FLOAT, false, this.fsize, 0);
+		gl.enableVertexAttribArray(this.a_Angle);
+
+		this.u_Warp = gl.getUniformLocation(gl.program, 'u_Warp');
+		this.u_Rotation = gl.getUniformLocation(gl.program, 'u_Rotation');
+	}
+
+	draw(){
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.gl_pos_buf);
+		gl.bufferData(gl.ARRAY_BUFFER, this.pos_buffer, gl.STATIC_DRAW);
+		gl.vertexAttribPointer(this.a_Position, this.p_fpv, gl.FLOAT, false, this.fsize*this.p_fpv, 0);
+		
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.gl_ang_buf);
+		gl.vertexAttribPointer(this.a_Angle, 1, gl.FLOAT, false, this.fsize, 0);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.gl_off_buf);
+		gl.bufferData(gl.ARRAY_BUFFER, this.off_buffer, gl.DYNAMIC_DRAW);
+		gl.vertexAttribPointer(this.a_Offset, 1, gl.FLOAT, false, this.fsize, 0);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.gl_opc_buf);
+		gl.bufferData(gl.ARRAY_BUFFER, this.opc_buffer, gl.DYNAMIC_DRAW);
+		gl.vertexAttribPointer(this.a_Opacity, 1, gl.FLOAT, false, this.fsize, 0);
+
+		gl.uniform1f(this.u_Warp, this.warp);
+		gl.uniform1f(this.u_Rotation, this.rotation);
+		gl.drawArrays(gl.LINES, 0, this.pos_buffer.length / this.p_fpv);
+	}
+
+	update(elapsed, fft){
+		this.warp = this.warp*.85 + exp_map(fft.sub_pro(0, .1), [0, 255], [0, 1], 2)*.15;
+
+		let rot_spd = 5000;
+		this.rot_time = (this.rot_time + elapsed) % rot_spd;
+		this.rotation = map(this.rot_time, [0, rot_spd], [0, Math.PI*2]);
+
+		let f_b = [.2, 1];
+		for(let i = 0; i < this.num; i++){
+			this.strands[i][2] += elapsed;
+			if(this.strands[i][2] > this.strands[i][1]){
+				this.strands[i][2] = 0;
+				this.strands[i][0] = (this.strands[i][0] + 1) % (2*this.len);
+			}
+			let drop_len = map(fft.sub_pro(map(i % 10, [0, 10], f_b), map(i % 10, [0, 10], f_b)), [0, 255], [this.len*.2, this.len*.5]);
+			for(let j = 0; j < 2*this.len; j++){
+				let d = this.strands[i][0] - j >= 0 ? this.strands[i][0] - j : this.strands[i][0] + 2*this.len - j;
+				let opacity = d == 0 ? .9 : exp_map(d, [0, drop_len], [.7, 0], .8);
+				let vertex_start = (i*2*this.len + j)*this.sym_len;
+				for(let k = 0; k < this.sym_len/this.p_fpv; k++){
+					this.opc_buffer[vertex_start + k] = opacity;
+					this.off_buffer[vertex_start + k] = (j % 2 == 0 ? -1 : 1) * this.offsets[d];
+				}
+			}
+		}
+		let sym_lvl = map(fft.sub_pro(0, .15), [0, 255], [0, this.symbols.length]);
+		for(let i = 0; i < 1000; i++){
+			let sym_ind = Math.floor(map(Math.random(), [0, 1], [0, this.curr_sym.length]));
+			let symbol = Math.floor(exp_map(Math.random(), [0, 1] , [0, sym_lvl], .5));
+			let pos_start = sym_ind*this.sym_len*this.p_fpv;
+			let transition = add(mult_scalar(this.symbols[symbol], 1.0), mult_scalar(this.symbols[this.curr_sym[sym_ind]], -1.0));
+			if(symbol != this.curr_sym[sym_ind]){
+				for(let j = 0; j < this.sym_len; j++){
+					this.pos_buffer[pos_start + j] += transition[j];
+				}
+				this.curr_sym[sym_ind] = symbol;
+			}
+		}
+
+	}
+}
+
